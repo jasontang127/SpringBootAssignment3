e27006e09e76cc3944b8a361e3f13293dfa7e29b
JustATangMan <jasontang127@gmail.com>
2023-06-24 19:12:19 -0400

    first commit, working service layer

--- src/main/java/com/jtang/assignment3/controllers/StudentRestController.java
+++ src/main/java/com/jtang/assignment3/controllers/StudentRestController.java
@@ -3,6 +3,7 @@ package com.jtang.assignment3.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,37 +11,37 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jtang.assignment3.entities.Student;
-import com.jtang.assignment3.repos.StudentRepository;
+import com.jtang.assignment3.services.StudentService;
 
 @RestController
 public class StudentRestController {
 
 	@Autowired
-	private StudentRepository repo;
+	private StudentService service;
 	
 	@RequestMapping(value = "/students/", method = RequestMethod.GET)
 	public List<Student> getStudents() {
-		return repo.findAll();
+		return service.getStudents();
 	}
 	
 	@RequestMapping(value = "/students/{id}", method = RequestMethod.GET)
 	public Student findStudent(@PathVariable("id") int id) {
-		return repo.findById(id).get();
+		return service.findStudent(id);
 	}
 	
 	@RequestMapping(value = "/students/", method = RequestMethod.POST)
 	public Student createStudent(@RequestBody Student student) {
-		return repo.save(student);
+		return service.createStudent(student);
 	}
 	
 	@RequestMapping(value = "/students/", method = RequestMethod.PUT)
 	public Student updateStudent(@RequestBody Student student) {
-		return repo.save(student);
+		return service.updateStudent(student);
 	}
 	
 	@RequestMapping(value = "/students/{id}", method = RequestMethod.DELETE)
 	public void deleteStudent(@PathVariable("id") int id) {
-		repo.deleteById(id);
+		service.deleteStudent(id);
 	}
 	
 }
