2b37628c69f728dfd24f71f3c38972c89057269f
JustATangMan <jasontang127@gmail.com>
2023-06-24 20:46:29 -0400

    first commit, working service layer (#1)
    * first commit, working service layer
    
    * renamed delete student method

---
+++ src/main/java/com/jtang/assignment3/services/StudentService.java
@@ -0,0 +1,18 @@
+package com.jtang.assignment3.services;
+
+import java.util.List;
+
+import com.jtang.assignment3.entities.Student;
+
+public interface StudentService {
+
+	public List<Student> getStudents();
+	
+	public Student findStudent(int id);
+	
+	public Student updateStudent(Student student);
+	
+	public Student createStudent(Student student);
+	
+	public void deleteStudent(int id);
+}
---
+++ src/main/java/com/jtang/assignment3/services/impl/DefaultStudentService.java
@@ -0,0 +1,44 @@
+package com.jtang.assignment3.services.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.jtang.assignment3.entities.Student;
+import com.jtang.assignment3.repos.StudentRepository;
+import com.jtang.assignment3.services.StudentService;
+
+@Service
+public class DefaultStudentService implements StudentService {
+
+	@Autowired
+	private StudentRepository repo;
+	
+	@Override
+	public List<Student> getStudents() {
+		return repo.findAll();
+	}
+	
+	@Override
+	public Student findStudent(@PathVariable("id") int id) {
+		return repo.findById(id).get();
+	}
+	
+	@Override
+	public Student createStudent(@RequestBody Student student) {
+		return repo.save(student);
+	}
+	
+	@Override
+	public Student updateStudent(@RequestBody Student student) {
+		return repo.save(student);
+	}
+	
+	@Override
+	public void deleteStudent(@PathVariable("id") int id) {
+		repo.deleteById(id);
+	}
+}
